25129c5314bc893abeba71b37e41741cb37c7e50
adhunna <adhunna@myseneca.ca>
2023-03-24 16:20:02 -0400

    sagar

--- blog-service.js
+++ blog-service.js
@@ -1,143 +1,62 @@
-const fs = require('fs');  
-var posts = [];
-var categories = [];
-
 exports.initialize = () =>{
-    return new Promise ((resolve, reject) =>{
-        fs.readFile('./data/posts.json', (err,data)=> {
-            if (err) 
-            {
-                reject ('unable to read file');
-            }
-            else 
-            {
-                posts = JSON.parse(data);
-            }
-        });
-        fs.readFile('./data/categories.json', (err,data) =>{
-            if (err) 
-            {
-                reject ('unable to read file');
-            }
-            else 
-            {
-                categories = JSON.parse(data);
-            }
-        })       
-        resolve();
-    })
+    return new Promise((resolve, reject) => {
+        reject();
+});
+
 };
 
 exports.getPostsByCategory = function(category){
     return new Promise((resolve, reject) => {
-        var Category_post = posts.filter(post => post.category == category);
-        if(Category_post.lenght != 0) 
-        {
-            resolve(Category_post);
-        }
-        else
-        {
-            reject("no result returned");
-        }
-    })
+        reject();
+});
+
 };
 
 exports.getPostsByMinDate = function(minDateStr){
     return new Promise((resolve, reject) => {
-        var MINdate = posts.filter(post => new Date(post.postDate) >= new Date(minDateStr));
-        if(MINdate.lenght != 0) 
-        {
-            resolve(MINdate);
-        }
-        else 
-        {
-            reject("no result returned");
-        }
-    })
+        reject();
+});
+
 };
 
 exports.getPostById = function(id) {
     return new Promise((resolve, reject) => {
-        var Post_id = posts.filter(post => post.id == id);
-        if(Post_id.lenght != 0) 
-        {
-            resolve(Post_id);
-        }
-        else 
-        {
-            reject("no result returned");
-        }
-    })
+        reject();
+});
+
 };
 
 exports.getAllPosts= function() {
     return new Promise((resolve, reject) => {
-        if (posts.length != 0) 
-        {
-            resolve(posts);
-        } 
-        else 
-        {
-            reject("No results returned");
-        }
-    })
+        reject();
+});
+
 };
 
 exports.addPost= function(postData) {
     return new Promise((resolve, reject) => {
-        postData.published == undefined ? postData.published = false : postData.published = true;
-        postData.id = posts.lenght + 1;
-        if (posts.length != 0) 
-        {
-            posts.push(postData);
-            resolve(postData);
-        } 
-        else 
-        {
-            reject("No results returned");
-        }
-    })
+        reject();
+});
+
 };
 
 exports.getCategories = function() {
-    return new Promise ((resolve,reject) => {
-        if (categories.length != 0) 
-        {
-            resolve(categories);
-        }
-        else 
-        {
-            reject('no result found');
-        }
-    })
+    return new Promise((resolve, reject) => {
+        reject();
+});
+
 };
 
 exports.getPublishedPosts = function() {
-    return new Promise ((resolve, reject) => {
-        var publish = posts.filter(post => post.published == true);
-        if (publish.length != 0) 
-        {
-            resolve(publish);
-        }
-        else
-        {
-            reject('no result found');
-        }
-        
-    })
+    return new Promise((resolve, reject) => {
+        reject();
+});
+
 };
 
 exports.getPublishedPostsByCategory = function(category) {
-    return new Promise ((resolve, reject) => {
-        var publish = posts.filter(post => post.published == true && post.category == category);
-        if (publish.length != 0) 
-        {
-            resolve(publish);
-        }
-        else
-        {
-            reject('no result found');
-        }
-        
-    })
+    return new Promise((resolve, reject) => {
+        reject();
+});
+
 }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: __Arjun_Sagar_Dhunna__ Student ID: __157099219__ Date: ________________
+*  Name: __Arjun_Sagar_Dhunna__ Student ID: __157099219__ Date: __2023-03-24__
 *
 *    Online (Cyclic) Link: https://dull-plum-xerus-toga.cyclic.app
 *
